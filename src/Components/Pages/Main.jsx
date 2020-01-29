05a53f46b66930d66cff48ea446fffaf2e42c754
RaphaelAGN <raphael.nogueira@sga.pucminas.br>
2020-01-29 13:46:02 -0200

    ajustes main

--- src/Components/Pages/Main.jsx
+++ src/Components/Pages/Main.jsx
@@ -14,16 +14,20 @@ const Main = () => {
                 <div>
                   <StyledH1>Nome</StyledH1>
                   <StyledP>Encontre monitores dentro da sua faculdade!</StyledP>
-                  <ImageDiv>
+                </div>
+                <div position="relative">  
                     <img src="https://play.google.com/intl/pt-BR/badges/static/images/badges/pt-br_badge_web_generic.png" 
                           alt="googlePlay"
                           width="150px"
                           position="absolute"></img>
+                  </div>
+                  <div position="relative">
                     <img src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" 
                           alt="appStore"
+                          margin-bottom="15px"
+                          margin-left="15px"
                           position="absolute"></img>
-                  </ImageDiv>
-                </div>
+                  </div>
             </div>
         </>
         </ThemeProvider>
@@ -31,48 +35,51 @@ const Main = () => {
 }
 
 const StyledH1 = styled.h1`
+  position: relative;
   @media only screen and (min-width: 600px) {
-    font-size: 30px;
-    margin-bottom: 20px;
-    margin-left: 10px;
+    font-size: 3vw;
+    margin-bottom: 2vw;
+    margin-left: 2vw;
   }
 
   @media only screen and (max-width: 600px) {
-    font-size: 20px;
-    margin-bottom: 20px;
-    margin-left: 10px;
+    font-size: 4vw;
+    margin-bottom: 2vw;
+    margin-left: 2vw;
   }
 
   @media only screen and (min-width: 1400px) {
-    font-size: 5vw;
-    margin-bottom: 20px;
-    margin-left: 10px;
+    font-size: 2vw;
+    margin-bottom: 2vw;
+    margin-left: 1vw;
   }
 `;
 
 const StyledP = styled.p`
-
-  @media screen and (min-width: 600px) {
-    font-size: 25px;
-    margin-bottom: 200px;
-    margin-right: 600px;
+  position: relative;
+  font-weight: bold;
+  @media screen and (min-width: 601px) {
+    font-size: 2vw;
+    margin-bottom: 12vw;
+    margin-right: 40vw;
   }
 
-  @media screen and (max-width: 601px) {
-    font-size: 15px;
-    margin-bottom: 50px;
-    margin-right: 50px;
+  @media screen and (max-width: 600px) {
+    font-size: 3vw;
+    margin-bottom: 15vw;
+    margin-right: 15vw;
   }
 
   @media only screen and (min-width: 1400px) {
-    font-size: 5vw;
-    margin-bottom: 20px;
-    margin-left: 10px;
+    font-size: 2vw;
+    margin-bottom: 15vw;
+    margin-right: 40vw;
   }
 `;
 
 const ImageDiv = styled.div`
   display: inline;
+  position: absolute;
 `;
 
 export default Main;
